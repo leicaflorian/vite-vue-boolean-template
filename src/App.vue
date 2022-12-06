323be269e5a581f771f2d9076f3bb08944dbb6dd
leicaflorian <florian.leica@gmail.com>
2022-12-06 01:52:46 +0100

    Updated template

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ welcome }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      welcome: "Welcome Boolean"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@use "./styles/general.scss";
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// import BS js
+// import "bootstrap";
+
+createApp(App).mount('#app')
